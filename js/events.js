3aebd14189c3636f2d9b52ccf5e67704a4e7169e
HP <HP@DESKTOP-8E0FCDH>
2025-05-23 00:52:08 +0600

    on click function calling

--- js/events.js
+++ js/events.js
@@ -1,1 +1,17 @@
 console.log("this is a separate js file");
+
+function clickMe() {
+  const num1Input = document.getElementById("number1");
+  const num2Input = document.getElementById("number2");
+  const sumInput = document.getElementById("sum");
+  const num1 = parseInt(num1Input.value);
+  const num2 = parseInt(num2Input.value);
+  const sum = num1 + num2;
+
+  sumInput.value = sum;
+  sumInput.style.backgroundColor = "red";
+  sumInput.style.color = "white";
+  console.log(sum);
+  console.log("ami bolod");
+  console.log("ami gadha");
+}
